9bc6679ae535db558ec431000afb6ea00a50228a
nehabalaji <nehabalaji10@gmail.com>
2022-07-05 19:38:29 +0530

    add login activity logic

--- app/src/main/java/com/miniproject/soi/LoginActivity.kt
+++ app/src/main/java/com/miniproject/soi/LoginActivity.kt
@@ -1,11 +1,56 @@
 package com.miniproject.soi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        mAuth = Firebase.auth
+        val emailET = findViewById<EditText>(R.id.etUsername);
+        val passwordEt = findViewById<EditText>(R.id.etPassword);
+        val loginBtn = findViewById<Button>(R.id.btnLogin);
+
+        loginBtn.setOnClickListener {
+            val email = emailET.text.toString()
+            val password = passwordEt.text.toString()
+            Log.v("SIGN IN", email+password)
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val intent = Intent(this, QuizActivity::class.java)
+                    startActivity(intent);
+                    finish()
+                }
+                else {
+                    Log.v("SIGN IN", it.exception.toString())
+                    Toast.makeText(this, "Check credentials!"+it.exception, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser: FirebaseUser? = mAuth.currentUser;
+        if(currentUser!=null) {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
